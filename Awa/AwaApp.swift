4d3f9b8f9d3ca859adeda845ed5bbe8e638287d4
Odeinjul <odeinjul@gmail.com>
2022-12-20 15:53:37 +0800

    Initial Commit

---
+++ Awa/AwaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwaApp.swift
+//  Awa
+//
+//  Created by Ode on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct AwaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Awa/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Awa
+//
+//  Created by Ode on 2022/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
